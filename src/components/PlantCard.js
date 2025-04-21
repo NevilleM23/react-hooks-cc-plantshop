50102fe8e409a5d27bb896a5ba8df20a4cb9a4d4
Nevillemuli <neville.muli@gmail.com>
2025-04-21 20:27:28 +0300

    finishes plantsy project

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,31 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({plant, onUpdatePlant}) {
+  const { id, name, image, price, soldOut } = plant;
+
+  const handleToggleSoldOut = () => {
+    const updatedSoldOut = !soldOut;
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ soldOut: updatedSoldOut }),
+    })
+      .then((r) => r.json())
+      .then((updatedPlant) => {
+        onUpdatePlant(updatedPlant);
+      });
+  };
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
+      <img src={image} alt={name} />
       <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <p>Price: {price}</p>
+      {soldOut? (
+        <button onClick={handleToggleSoldOut} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleSoldOut}> Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => {
+        const updatedPlants = data.map((plant) => ({
+          ...plant,
+          soldOut: plant.soldOut || false,
+        }));
+        setPlants(updatedPlants);
+      });
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    setPlants([...plants, newPlant]);
+  };
+
+  const handleUpdatePlant = (updatedPlant) => {
+    setPlants((prevPlants) =>
+      prevPlants.map((plant) =>
+        plant.id === updatedPlant.id ? updatedPlant : plant
+      )
+    );
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm}  onSearchChange={setSearchTerm}/>
+      <PlantList plants={filteredPlants} onUpdatePlant={handleUpdatePlant} />
     </main>
   );
 }
